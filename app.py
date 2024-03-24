af82c370dcf3dbdb32c83120ac919bfa5a700728
abdul1251 <99087722+abdul1251@users.noreply.github.com>
2024-03-24 12:38:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import numpy as np
+import pickle
+import pandas as pd
+
+def load_model(modelfile):
+	loaded_model = pickle.load(open(modelfile, 'rb'))
+	return loaded_model
+
+
+st.title("Predicting Whether a user will Bounce or Not")
+data = st.file_uploader("Upload an File", type=["csv"])
+
+if st.button('Predict'):
+
+    # read dataset
+    df = pd.read_csv(data)
+
+    df = df.dropna()
+    # Convert 'Date' column to datetime format
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    # Feature Engineering: Extract additional features from the 'Date' column
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day
+
+    # loading Model
+    loaded_model = load_model('model.pkl')
+
+
+    result = loaded_model.predict(df)
+
+    df['User_Bounce'] = result
+    st.dataframe(df)
